d1d986e8f5bc3632cdc966b75659708d9616e378
CHamat96 <coreyhamat@gmail.com>
2023-02-03 13:02:03 -0500

    fix temp copy styling

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -69,8 +69,9 @@ const GlobalStyles = createGlobalStyle`
 
     .temp {
         font-family:var(--tempFont);
-        font-weight:600;
+        font-weight:500;
         font-size:2.8rem;
+        letter-spacing:0.05rem;
         color:var(--black);
         &.todayTemp {
             font-size:5rem;
